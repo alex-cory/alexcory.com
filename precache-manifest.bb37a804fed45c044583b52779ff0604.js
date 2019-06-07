11f37272efae92e5ed71775190a5f0114f8743dd
Alex Cory <alexcory@fb.com>
2019-06-07 12:27:34 -0700

    Updates

---
+++ precache-manifest.bb37a804fed45c044583b52779ff0604.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2f25229ba2e55a96f69529e7e03be4",
+    "url": "/alexcory.com/index.html"
+  },
+  {
+    "revision": "d55d96973fa53fd86c3d",
+    "url": "/alexcory.com/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/alexcory.com/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d55d96973fa53fd86c3d",
+    "url": "/alexcory.com/static/js/main.8eb54a3c.chunk.js"
+  },
+  {
+    "revision": "1b466e211048beaac59d",
+    "url": "/alexcory.com/static/js/runtime~main.a1c497f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/alexcory.com/static/media/logo.5d5d9eef.svg"
+  }
+]);
