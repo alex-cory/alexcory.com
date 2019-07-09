6deef30bf56defca1bb834a6edf8bbd23edd3560
Alex Cory <alexcory@fb.com>
2019-07-09 12:46:44 -0700

    updating links

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -20,13 +20,13 @@ export const SocialMedia = () => (
     <SocialLink type='github' url='https://github.com/alex-cory' />
     <SocialLink type='linkedin' url='https://linkedin.com/in/alexcory' />
     <SocialLink type='codepen' url='https://codepen.io/alex-cory/' />
-    <SocialLink type='twitter' url='twitter.com/alexcory_' />
+    <SocialLink type='twitter' url='https://twitter.com/alexcory_' />
     <SocialLink type='stackoverflow' url='https://stackoverflow.com/users/2782583/alex-cory' />
-    <SocialLink type='facebook' url='fb.com/alexlcory' />
-    <SocialLink type='medium' url='https://medium.com/alexcory' />
+    <SocialLink type='facebook' url='https://fb.com/alexlcory' />
+    <SocialLink type='medium' url='https://medium.com/@alexcory' />
     <SocialLink type='jsfiddle' url='https://jsfiddle.net/user/alexcory/fiddles/' />
     <SocialLink type='email' url='mailto:results@alexcory.com' />
     {/* Runkit: https://runkit.com/alex-cory */}
-    {/* Codesandbox: https://codesandbox.io/dashboard/recent */}
+    {/* Codesandbox: https://codesandbox.io/u/alex-cory */}
   </Container>
 )
